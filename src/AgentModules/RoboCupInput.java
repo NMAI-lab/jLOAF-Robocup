0b20a94c257ec915e71a37a6c0e677d6326a6787
sachag678 <sacha.gunaratne@gmail.com>
2017-05-17 11:55:17 -0400

    Removed similarityMetricStrategy override methods
    Removed redundant code

--- src/AgentModules/RoboCupInput.java
+++ src/AgentModules/RoboCupInput.java
@@ -10,39 +10,9 @@ public class RoboCupInput extends ComplexInput {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static SimilarityMetricStrategy simMet;
 	
-	public RoboCupInput(String s) {
-		super(s);
-	}
-	
-	@Override
-	public double similarity(Input i) {
-		//See if the user has defined similarity for each specific input, for all inputs
-		//  of a specific type, of deferred to superclass
-		if(this.simStrategy != null){
-			return simStrategy.similarity(this, i);
-		}else if(RoboCupInput.isClassStrategySet()){
-			return RoboCupInput.similarity(this, i);
-		}else{
-			return super.similarity(i);
-		}
-	}
-
-	private static double similarity(Input complexInput, Input i) {
-		return RoboCupInput.simMet.similarity(complexInput, i);
-	}
-
-	public static boolean isClassStrategySet(){
-		if(RoboCupInput.simMet == null){
-			return false;
-		}else{
-			return true;
-		}
-	}
-
-	public static void setClassSimilarityMetric(SimilarityMetricStrategy s){
-		RoboCupInput.simMet = s;
+	public RoboCupInput(String s, SimilarityMetricStrategy sim) {
+		super(s, sim);
 	}
 
 }
