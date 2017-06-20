1915f478cd26f9719691c1a63c63bcba96caa4e4
ali fawaz <alifawaz@cmail.carleton.ca>
2017-06-20 11:20:28 -0400

    change RobocupAction
    1- made RobocupAction extend ComplexAction instead of atomic Action
    because of the changes done for atomic Action

--- src/AgentModules/RoboCupAgent.java
+++ src/AgentModules/RoboCupAgent.java
@@ -1,23 +1,12 @@
 package AgentModules;
 import org.jLOAF.Agent;
-import org.jLOAF.MotorControl;
-import org.jLOAF.Perception;
-import org.jLOAF.Reasoning;
-import org.jLOAF.action.Action;
+
 import org.jLOAF.casebase.CaseBase;
-import org.jLOAF.inputs.AtomicInput;
-import org.jLOAF.inputs.ComplexInput;
+
 import org.jLOAF.inputs.Input;
-import org.jLOAF.reasoning.SimpleKNN;
+
 import org.jLOAF.reasoning.WeightedKNN;
-import org.jLOAF.sim.atomic.Equality;
-import org.jLOAF.sim.atomic.EuclideanDistance;
-import org.jLOAF.sim.atomic.PercentDifference;
-import org.jLOAF.sim.complex.AuctionMaximalMatching;
-import org.jLOAF.sim.complex.GreedyMunkrezMatching;
-import org.jLOAF.sim.complex.Mean;
-import org.jLOAF.sim.complex.WeightedMean;
-import org.jLOAF.weights.SimilarityWeights;
+
 
 public class RoboCupAgent extends Agent {
 
